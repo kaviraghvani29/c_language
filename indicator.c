a03017d9dd895509b07cc4c3c6d833e037c68d5e
kavita <kavitaraghvani2@gmail.com>
2025-08-19 13:31:09 +0530

    indicator 21.1

---
+++ indicator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    char *p;
+    int length = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    p = str;
+
+    while (*p != '\0') 
+    {  
+        length++;
+        p++;
+    }
+
+    printf("Length of the string is: %d\n", length);
+
+    return 0;
+}
