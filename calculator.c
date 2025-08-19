e37ec9f47cd5d6a335a9a568199b24ce1559f005
kavita <kavitaraghvani2@gmail.com>
2025-08-19 13:25:08 +0530

    calculator 19.1

--- calculator.c
+++ calculator.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// User defined functions
+// functions
 int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
 int mul(int a, int b) { return a * b; }
